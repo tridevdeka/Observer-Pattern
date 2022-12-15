0c4343ad5cfce7ee7275d6a73dbca7e50e762ab3
tri_dev <tridev@myeglu.com>
2022-12-15 06:21:16 +0530

    Observer pattern with Livedata,Stateflow,FLow,SharedFlow

---
+++ app/src/main/java/com/tridev/observablepatterns/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.tridev.observablepatterns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.*
+import com.google.android.material.snackbar.Snackbar
+import com.tridev.observablepatterns.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        mainViewModel=ViewModelProvider(this).get(MainViewModel::class.java)
+
+        mBinding.btLivedata.setOnClickListener {
+            mainViewModel.triggerLiveData()
+        }
+
+        mBinding.btStateflow.setOnClickListener {
+            mainViewModel.triggerStateFlow()
+        }
+
+        mBinding.btFlow.setOnClickListener {
+            lifecycleScope.launch {
+                repeatOnLifecycle(Lifecycle.State.STARTED) {
+                    mainViewModel.triggerFLow().collectLatest {
+                        mBinding.tvFlow.text = it
+                    }
+                }
+            }
+        }
+
+        mBinding.btSharedFlow.setOnClickListener {
+            mainViewModel.triggerSharedFlow()
+        }
+
+        subscribeToObservables()
+    }
+
+    private fun subscribeToObservables() {
+        //observe() is called whenever there is a change in livedata value
+        mainViewModel.liveData.observe(this) {
+            mBinding.tvLivedata.text = it
+        }
+
+        /*Hot flow -> Keep emitting values even though there are no collectors
+        *Stateflow SharedFlow are Hot flows
+        *Cold flow-> Won't emit any values if there is not any collector
+        */
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.stateFlow.collectLatest {
+                    Snackbar.make(mBinding.root, it, Snackbar.LENGTH_LONG).show()
+                    mBinding.tvStateflow.text = it
+                }
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.sharedFLow.collectLatest {
+                    Snackbar.make(mBinding.root, it, Snackbar.LENGTH_LONG).show()
+                    mBinding.tvSharedFlow.text = it
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/tridev/observablepatterns/MainViewModel.kt
@@ -0,0 +1,85 @@
+package com.tridev.observablepatterns
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+
+    /*
+    * The ViewModel class is a business logic or screen level state holder.
+    *  It exposes state to the UI and encapsulates related business logic.
+    *  Its principal advantage is that it caches state and persists it through configuration changes.
+    * */
+
+    /*Livedata is a lifecycle aware observable data holder,it holds ui state in the end.
+    *If any value in ui changes over time then that can be stored in liveData,
+    otherwise changed value will be lost when configuration change happens.
+     */
+
+    private val _liveData = MutableLiveData("Hello World")
+    val liveData: LiveData<String> = _liveData
+
+    /*Stateflow is a state-holder observable flow that emits the current and new state updates to its collectors.
+   *The current state value can also be read through its value property.
+   * To update state and send it to the flow, assign a new value to the value property of the MutableStateFlow class.
+   *It can also perform mapping with the emitted states.
+   ***It triggers EVENT for the updated state every time activity is created.
+   * */
+    private val _stateFlow = MutableStateFlow("Hello World State flow")
+    val stateFlow = _stateFlow.asStateFlow()
+
+
+    private val _sharedFlow = MutableSharedFlow<String>()
+    val sharedFLow = _sharedFlow.asSharedFlow()
+
+    /*SharedFlow is a state-holder observable flow that emits the current and new state updates to its collectors.
+       *The current state value can also be read through its value property.
+       * To update state and send it to the flow, assign a new value to the value property of the MutableStateFlow class.
+       *It can also perform mapping with the emitted states.
+       ***It triggers only ONE TIME EVENT for the updated value unlike Stateflow
+       */
+
+    fun triggerLiveData() {
+        /*
+        *main thread always overrides the setValue()'s value with postValue().
+        *_liveData.postValue("a")
+        *_liveData.setValue("b")
+        * a value overrides b value by the main thread.
+        */
+        _liveData.postValue("Live Data")
+    }
+
+
+    fun triggerStateFlow() {
+        _stateFlow.value = "State Flow"
+    }
+
+
+    /*
+    * Flow is not state holder,does not hold ui state or store values.
+    * On configuration change value is lost.
+    * FLow does not hold any value it just performs to emit values and returns Flow
+    * */
+
+    fun triggerFLow(): Flow<String> {
+        return flow {
+            repeat(5) {
+                emit("Item $it")
+                delay(1000L)
+            }
+        }
+    }
+
+    fun triggerSharedFlow() {
+        viewModelScope.launch {
+            _sharedFlow.emit("Shared flow")
+        }
+    }
+
+
+}
